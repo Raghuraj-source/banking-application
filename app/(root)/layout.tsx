873f3f01c10ed4a271c2c613eab84ff2514be0e6
archana <thaarchana7jan@gmail.com>
2024-08-02 18:40:04 +0530

    feat: Add pages for My Banks, Payment Transfer, and Transaction History
    This commit adds new pages for My Banks, Payment Transfer, and Transaction History. Each page is a React functional component that renders a simple placeholder text. These pages will serve as the foundation for future development and integration with backend APIs.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,47 @@
 
-export default function RootLayout({
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn: User = {
+    firstName: 'Raghuraj',
+    lastName: 'Developer',
+    $id: '',
+    email: '',
+    userId: '',
+    dwollaCustomerUrl: '',
+    dwollaCustomerId: "",
+    address1: "",
+    city: "",
+    state: "",
+    postalCode: "",
+    dateOfBirth: "",
+    ssn: ""
+  };
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src= "/icons/logo.svg" width={30} height={30} alt= "logo" />
+          <div>
+            <MobileNav user= {loggedIn }/>
+          </div>
+
+        </div>
+
         {children}
+      </div>
+      
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Raghuraj", lastName: 'Developer', email: 'raghurajt85@gmail.com'};
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-export default Home
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1} // or any other non-empty expression
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+         RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar  
+       user={loggedIn}
+       transactions={[]}
+       banks={[{ currentBalance:124.56}, {currentBalance:524.55}]}
+
+      />
+    </section>
+  );
+};
+
+export default Home;
